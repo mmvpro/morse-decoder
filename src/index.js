9cf66b44983a3e16d5e17156ecbd7f20f0ad2980
mmvpro <dublibum@gmail.com>
2023-01-27 21:11:21 +0300

    done Morse-decoder

--- src/index.js
+++ src/index.js
@@ -38,8 +38,55 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    const morse = {
+        "0000001011": MORSE_TABLE[".-"],
+        "0011101010": MORSE_TABLE["-..."],
+        "0011101110": MORSE_TABLE["-.-."],
+        "0000111010": MORSE_TABLE["-.."],
+        "0000000010": MORSE_TABLE["."],
+        "0010101110": MORSE_TABLE["..-."],
+        "0000111110": MORSE_TABLE["--."],
+        "0010101010": MORSE_TABLE["...."],
+        "0000001010": MORSE_TABLE[".."],
+        "0010111111": MORSE_TABLE[".---"],
+        "0000111011": MORSE_TABLE["-.-"],
+        "0010111010": MORSE_TABLE[".-.."],
+        "0000001111": MORSE_TABLE["--"],
+        "0000001110": MORSE_TABLE["-."],
+        "0000111111": MORSE_TABLE["---"],
+        "0010111110": MORSE_TABLE[".--."],
+        "0011111011": MORSE_TABLE["--.-"],
+        "0000101110": MORSE_TABLE[".-."],
+        "0000101010": MORSE_TABLE["..."],
+        "0000000011": MORSE_TABLE["-"],
+        "0000101011": MORSE_TABLE["..-"],
+        "0010101011": MORSE_TABLE["...-"],
+        "0000101111": MORSE_TABLE[".--"],
+        "0011101011": MORSE_TABLE["-..-"],
+        "0011101111": MORSE_TABLE["-.--"],
+        "0011111010": MORSE_TABLE["--.."],
+        1011111111: MORSE_TABLE[".----"],
+        1010111111: MORSE_TABLE["..---"],
+        1010101111: MORSE_TABLE["...--"],
+        1010101011: MORSE_TABLE["....-"],
+        1010101010: MORSE_TABLE["....."],
+        1110101010: MORSE_TABLE["-...."],
+        1111101010: MORSE_TABLE["--..."],
+        1111111010: MORSE_TABLE["---.."],
+        1111111110: MORSE_TABLE["----."],
+        1111111111: MORSE_TABLE["-----"],
+        "**********": " ",
+      };
+      
+      
+        let result = [];
+        for (let i = 0; i < expr.length; i += 10) {
+          result.push(expr.substring(i, i + 10));
+        }
+        return result.map((e) => morse[e]).join("");
+      }
+      
+
 
 module.exports = {
     decode
